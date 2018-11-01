aef781cd0169030bf968f1b17d2a37391898fa24
zyken1 <35464034+zyken1@users.noreply.github.com>
2018-11-01 10:28:49 -0600

    cambios1

--- Empleado/index.php
+++ Empleado/index.php
@@ -12,7 +12,7 @@
   <!-- <caption>List of users</caption> -->
   <thead class="alert-info shadow text-center">
     <tr>
-      <th scope="col" colspan="2">Bienvenido(a): <?php echo $_SESSION["Nombre"]; ?></th>
+      <th scope="col" colspan="3">Bienvenido(a): <?php echo $_SESSION["Nombre"]; ?></th>
       <!-- <th scope="col">First</th> -->
     </tr>
   </thead>
@@ -20,10 +20,12 @@
     <tr>
       <th scope="row">Tu correo personal es:</th>
       <td> <?php echo $_SESSION["Correo"]; ?></td>
+      <td></td>
     </tr>
     <tr>
       <th scope="row">Correo de Cotizaciones y General</th>
       <td><?php echo $_SESSION["Correo2"]; ?></td>
+      <td></td>
     </tr>
   </tbody>
 </table>
--- js/Empleado/Empleado_Funciones.js
+++ js/Empleado/Empleado_Funciones.js
@@ -5,22 +5,26 @@ function clickIE4()
 	}
 
 //Función para impedir que el usuario de click derecho en la página para NetScape
-function clickNS4(e)
-	{
-		if (document.layers||document.getElementById&&!document.all)
-			if (e.which==2||e.which==3) return false;
-	}
 
-if (document.layers)
-	{
-		document.captureEvents(Event.MOUSEDOWN);
-		document.onmousedown=clickNS4;
-	}
-else if (document.all&&!document.getElementById)
-	{
-		document.onmousedown=clickIE4;
-	}
-document.oncontextmenu=new Function("return false");
+
+
+// function clickNS4(e)
+// 	{
+// 		if (document.layers||document.getElementById&&!document.all)
+// 			if (e.which==2||e.which==3) return false;
+// 	}
+
+// if (document.layers)
+// 	{
+// 		document.captureEvents(Event.MOUSEDOWN);
+// 		document.onmousedown=clickNS4;
+// 	}
+// else if (document.all&&!document.getElementById)
+// 	{
+// 		document.onmousedown=clickIE4;
+// 	}
+// document.oncontextmenu=new Function("return false");
+
 
 //Función que solo permite ingresar letras, es decir, no permite la entrada de numeros
 function Solo_Letras()
